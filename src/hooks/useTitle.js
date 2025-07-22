efe8955afa6731225f36d9c4bb7a22ee6083b7c4
SayuruDev <mailtosayuru@gmail.com>
2025-07-22 13:10:46 -0400

    CardCard is Added to the Cart

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,11 +1,14 @@
 
 
-export const useTitle = () => {
-  return (
-    <div>
-      null
-    </div>
-  )
+import { useEffect } from "react";
+
+export const useTitle = (title) => {
+
+  useEffect(() => {
+    document.title = `${title} | Shopping Cart`
+  }, [title]);
+
+  return null;
 }
 
 
